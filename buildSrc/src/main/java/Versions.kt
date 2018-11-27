71ec80d4ab7c6e808ea7c271c3d1af87e1004ca8
Pavel Savinov <swapii@gmail.com>
2018-11-28 00:00:32 +0300

    Android Gradle plugin 3.3.0-rc01

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -8,7 +8,7 @@ object Versions {
 
     const val androidSupport = "28.0.0"
     const val androidArchLifecycle = "1.1.1"
-    const val androidGradlePlugin = "3.3.0-beta04"
+    const val androidGradlePlugin = "3.3.0-rc01"
 
     const val dagger = "2.19"
 
