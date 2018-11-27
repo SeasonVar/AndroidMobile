4c16ae9445edc72eca2dcd6663e1d81d6c86ec60
Pavel Savinov <swapii@gmail.com>
2018-11-28 00:37:28 +0300

    Show authorization error

--- presentation/login/src/main/java/seasonvar/android/mobile/presentation/login/LoginViewModel.kt
+++ presentation/login/src/main/java/seasonvar/android/mobile/presentation/login/LoginViewModel.kt
@@ -7,6 +7,7 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.schedulers.Schedulers
 import seasonvar.android.mobile.domain.AuthGateway
+import seasonvar.android.mobile.domain.WrongAuthCredentialsException
 import javax.inject.Inject
 
 class LoginViewModel @Inject constructor(
@@ -17,6 +18,7 @@ class LoginViewModel @Inject constructor(
     val login = ObservableField<String>("")
     val password = ObservableField<String>("")
     val isProgress = ObservableBoolean()
+    val error = ObservableField<Int>(R.string.empty)
 
     private val disposables = CompositeDisposable()
 
@@ -26,9 +28,13 @@ class LoginViewModel @Inject constructor(
 
         authGateway.login(login.get()!!, password.get()!!)
             .subscribeOn(Schedulers.io())
-            .doOnComplete { router.userLoggedIn() }
-            .doOnError { TODO("Show error") }
-            .subscribe()
+            .subscribe(router::userLoggedIn) {
+                isProgress.set(false)
+                when (it) {
+                    is WrongAuthCredentialsException -> R.string.wrong_login_or_password
+                    else -> R.string.cant_authorize
+                }.apply { error.set(this) }
+            }
             .addTo(disposables)
 
     }
